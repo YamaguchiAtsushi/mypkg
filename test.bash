081dfe1d88d9ed0d960dd260be0caf862488b52a
Atsushi Yamaguchi <yamaguchi.atsushi2000@gmail.com>
2022-08-08 15:32:17 +0900

    improve the test

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash -xv
 
+ng () {
+    echo NG at LINE $1
+    res=1
+}
+
+res=0
+
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+### I/O TEST ###
+
+[ "${out}" = 14 ] || ng $LINENO
+
+[ "$res" = 0 ] && echo OK
+exit $res
